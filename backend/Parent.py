cfb52dea1c94b161f328c9e0423436cf0fa59289
djangg <djang2@umbc.edu>
2025-04-01 13:02:19 -0400

    Update Parent.py

--- backend/Parent.py
+++ backend/Parent.py
@@ -1,7 +1,28 @@
 class Parent:
-    def __init__(self, name, relationship, phone_number, password):
-        self.name = name
+    def __init__(self,  first_name, last_name, relationship, phone_number, password, email_address):
+        self.email_address = email_address
+        self.first_name = first_name
+        self.last_name = last_name
         self.relationship = relationship
         self.phone_number = phone_number
         self.password = password
         pass
+
+    def get_first_name(self):
+        return self.first_name
+    
+    def get_last_name(self):
+        return self.last_name
+    
+    def get_full_name(self):
+        return self.first_name + " " + self.last_name
+    
+    def get_relationship(self):
+        return self.relationship
+    
+    def get_phone_number(self):
+        return self.phone_number
+    
+    def get_password(self):
+        return self.password
+    
