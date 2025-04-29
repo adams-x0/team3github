1059832fe2c2facad2e7a95311598469ee9e47e5
Adams <92267144+adams-x0@users.noreply.github.com>
2025-04-29 08:51:04 -0400

    book session and find therapist section joined on student dashboard

--- frontend/src/components/StudentDashboard.jsx
+++ frontend/src/components/StudentDashboard.jsx
@@ -15,8 +15,17 @@ import {
     Radio,
     RadioGroup,
     FormControlLabel,
+    Paper,
+    Table,
+    TableHead,
+    TableRow,
+    TableCell,
+    TableBody,
+    TableContainer,
 } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
@@ -25,7 +34,11 @@ import { bookAppointment } from "../Slices/SetSlice";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import dayjs from "dayjs";
+import isBetween from "dayjs/plugin/isBetween";
+import isoWeek from "dayjs/plugin/isoWeek";
 
+dayjs.extend(isBetween);
+dayjs.extend(isoWeek);
 
 const StudentDashboard = () => {
     const navigate = useNavigate();
@@ -33,8 +46,9 @@ const StudentDashboard = () => {
     const [therapists, setTherapists] = useState([]);
     const [selectedTherapistId, setSelectedTherapistId] = useState(null);
     const [selectedTime, setSelectedTime] = useState('');
+    const [sortBy, setSortBy] = useState('first_name'); // Default sorting by first name
+    const [sortOrder, setSortOrder] = useState('asc'); // Default sorting order
     const user = useSelector((state) => state.auth.user);
-    console.log(user)
 
     useEffect(() => { 
         if (!user) {
@@ -50,6 +64,49 @@ const StudentDashboard = () => {
     useEffect(() => {
         fetchAllTherapists(setTherapists);
     }, [setTherapists]);
+
+    const handleSort = (column) => {
+        if (sortBy === column) {
+            setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc');
+        } else { 
+            setSortBy(column);
+            setSortOrder('asc');
+        }
+    };
+
+    const sortedTherapists = [...therapists].sort((a, b) => { 
+        if (a[sortBy] < b[sortBy]) return sortOrder === 'asc' ? -1 : 1;
+        if (a[sortBy] > b[sortBy]) return sortOrder === 'asc' ? 1 : -1;
+        return 0;
+    });
+
+    const selectedTherapist = therapists.find(
+        (therapist) => String(therapist.therapist_id) === String(selectedTherapistId)
+    );
+
+    const availableDays = selectedTherapist ? Object.keys(JSON.parse(selectedTherapist.availability || '{}')) : [];
+
+    const currentMonday = dayjs().startOf('isoWeek');
+    const twoWeeksLater = currentMonday.add(13, 'day');
+
+    const shouldDisableDate = (date) => { 
+        const isPast = date.isBefore(dayjs(), 'day');
+        const isOutOfRange = !date.isBetween(currentMonday.subtract(1, 'day'), twoWeeksLater.add(1, 'day'), null, '[]'); 
+        const dayName = date.format('dddd');
+        const isUnavailable = !availableDays.includes(dayName);
+        return isPast || isOutOfRange || isUnavailable;
+    }
+
+    const dayOfWeek = selectedDate ? selectedDate.format('dddd') : null;
+    let availableTimes = [];
+    if (selectedTherapist && selectedTherapist.availability) {
+        try {
+            const availabilityObj = JSON.parse(selectedTherapist.availability);
+            availableTimes = availabilityObj[dayOfWeek] || [];
+        } catch (err) {
+            console.error('Failed to parse availability JSON:', err);
+        }
+    }
 
     const handleBookSession = async () => {
         if (!selectedTherapistId || !selectedTime) {
@@ -73,23 +130,6 @@ const StudentDashboard = () => {
         }
     };
 
-    const selectedTherapist = therapists.find(
-        (therapist) => String(therapist.therapist_id) === String(selectedTherapistId)
-    );
-
-    const dayOfWeek = selectedDate ? selectedDate.format('dddd') : null;
-    let availableTimes = [];
-    if (selectedTherapist && selectedTherapist.availability) {
-        try {
-            const availabilityObj = JSON.parse(selectedTherapist.availability);
-            availableTimes = availabilityObj[dayOfWeek] || [];
-        } catch (err) {
-            console.error('Failed to parse availability JSON:', err);
-        }
-    }
-
-
-
     return (
         <div>
             <StudentNavbar />
@@ -99,7 +139,7 @@ const StudentDashboard = () => {
                         Welcome, {user.first_name} {user.last_name}
                     </Typography>
 
-                {/* Book a Session */}
+                {/* Book a Session & Find Therapist */}
                 <Accordion defaultExpanded sx={{ mb: 5 }}>
                         <AccordionSummary expandIcon={<ExpandMoreIcon />}>
                             <Typography variant="h6">Book a Session</Typography>
@@ -111,54 +151,61 @@ const StudentDashboard = () => {
                                     <DateCalendar
                                         value={selectedDate}
                                         onChange={(newValue) => setSelectedDate(newValue)}
+                                        shouldDisableDate={shouldDisableDate}
                                     />
                                 </LocalizationProvider>
-
-                                {/* Therapist Selection */}
-                                <Box ml={{ md: 4 }} mt={{ xs: 2, md: 0 }} flexGrow={1}>
-                                    <Typography variant="subtitle1" gutterBottom>
-                                        Select a Therapist:
-                                    </Typography>
-                                    <RadioGroup
-                                        value={selectedTherapistId}
-                                        onChange={(e) => setSelectedTherapistId(e.target.value)}
-                                    >
-                                        {therapists.map((therapist) => (
-                                            <FormControlLabel
-                                                key={therapist.therapist_id}
-                                                value={therapist.therapist_id}
-                                                control={<Radio />}
-                                                label={`${therapist.first_name} ${therapist.last_name} — ${therapist.specialization}`}
-                                            />
-                                        ))}
-                                    </RadioGroup>
-                                    {availableTimes.length > 0 ? (
-                                        <Box mt={3}>
-                                            <Typography variant="subtitle1">Available times for {selectedDate.format("MMMM Do, YYYY")}:</Typography>
-                                            <RadioGroup
-                                                value={selectedTime}
-                                                onChange={(e) => setSelectedTime(e.target.value)}
-                                                row
-                                            >
-                                                {availableTimes.map((time) => (
-                                                    <FormControlLabel
-                                                        key={time}
-                                                        value={time}
-                                                        control={<Radio />}
-                                                        label={time}
-                                                    />
-                                                ))}
-                                            </RadioGroup>
-                                        </Box>
-                                    ) : <Box mt={3}>
-                                            <Typography variant="subtitle1">
-                                                Available times for {selectedDate.format("MMMM Do, YYYY")}: None
-                                            </Typography>
-                                        </Box>
-                                    }
-                                </Box>
                             </Box>
 
+                            {/* Therapist Table */}
+                            <box flexGrow={1}>
+
+                                <Typography variant="h6" mb={2}>
+                                    Find a Therapist
+                                </Typography>
+
+                                <TableContainer component={Paper}>
+                                    <Table>
+                                        <TableHead>
+                                            <TableRow>
+                                                <TableCell onClick={() => handleSort('first_name')} style={{ cursor: 'pointer' }}>
+                                                     First Name {sortBy === 'first_name' && (sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="small" /> : <ArrowDownwardIcon fontSize="small" />)}
+                                                </TableCell>
+                                                <TableCell onClick={() => handleSort('last_name')} style={{ cursor: 'pointer' }}>
+                                                    Last Name {sortBy === 'last_name' && (sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="small" /> : <ArrowDownwardIcon fontSize="small" />)}
+                                                </TableCell>
+                                                <TableCell onClick={() => handleSort('specialization')} style={{ cursor: 'pointer' }}>
+                                                    Specialization {sortBy === 'specialization' && (sortOrder === 'asc' ? <ArrowUpwardIcon fontSize="small" /> : <ArrowDownwardIcon fontSize="small" />)}
+                                                </TableCell>
+                                                <TableCell>Select</TableCell>
+                                            </TableRow>
+                                        </TableHead>
+
+                                        <TableBody>
+                                            {sortedTherapists.map((therapist) => (
+                                                <TableRow
+                                                    key={therapist.therapist_id}
+                                                    hover
+                                                    onClick={() => setSelectedTherapistId(therapist.therapist_id)}
+                                                    sx={{cursor: 'pointer'}}
+                                                >
+                                                    <TableCell>{therapist.first_name}</TableCell>
+                                                    <TableCell>{therapist.last_name}</TableCell>
+                                                    <TableCell>{therapist.specialization}</TableCell>
+                                                    <TableCell>
+                                                        <Button
+                                                            onClick={() => setSelectedTherapistId(therapist.therapist_id)}
+                                                            variant={selectedTherapistId === therapist.therapist_id ? "contained" : "outlined"}
+                                                            
+                                                        >
+                                                            Select  
+                                                        </Button>
+                                                    </TableCell>
+                                                </TableRow>
+                                            ))}
+                                        </TableBody>
+                                    </Table>
+                                </TableContainer>
+                            </box>
                             <Box mt={2}>
                                 <Button variant="contained" color="primary" onClick={handleBookSession}>
                                     Book Session
@@ -220,36 +267,7 @@ const StudentDashboard = () => {
                     </AccordionDetails>
                 </Accordion>
 
-                {/* Find Therapist */}
-                <Accordion sx={{ mb: 5 }}>
-                    <AccordionSummary expandIcon={<ExpandMoreIcon />}>
-                        <Typography variant="h6">Find Therapist</Typography>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                        <TextField label="Search by name, specialization ..." fullWidth />
-                        <Box mt={2}>
-                            <List>
-                                <ListItem
-                                    onClick={() => navigate('/student-dashboard')}
-                                    sx={{
-                                        border: '1px solid #ccc',
-                                        borderRadius: 2,
-                                        mb: 1,
-                                        transition: 'background-color 0.3s',
-                                        '&:hover': { backgroundColor: 'action.hover',
-                                        },
-                                        '&:focus': {
-                                            backgroundColor: 'primary.main',
-                                            color: 'white',
-                                        }
-                                    }}
-                                    secondaryAction={<Button variant="outlined">View Profile</Button>}>
-                                <ListItemText primary="Dr. Jane Doe" secondary="Specialization: Physical Therapy"/>
-                            </ListItem>
-                            </List>
-                        </Box>
-                    </AccordionDetails>
-                </Accordion>
+                
                 </Box>
             </Container>
         </div>
