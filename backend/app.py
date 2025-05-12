e4af195e81b9739bd15a4c39282c666fef5b5809
Caleb C <57231403+CalebC277@users.noreply.github.com>
2025-05-12 18:13:33 -0400

    Update app.py
    Added email credentials for email sending after appointment info

--- backend/app.py
+++ backend/app.py
@@ -8,6 +8,11 @@ from flask_mail import Mail, Message  # type: ignore
 
 app = Flask(__name__)
 CORS(app)  # Allow frontend to make requests
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # Or your SMTP server
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'cmsc447team32025@example.com'
+app.config['MAIL_PASSWORD'] = 'qpgtdwnjiugqozos'
 
 # Sanitize only string fields
 def safe_escape(value):
